c02de07a2b2b6fa06d70cf0394b048296b950b80
anna <adyanovna@gmail.com>
2023-11-12 18:09:30 +0700

    Задание 2.1 2 2/3 сделала кнопку с функционалом.

---
+++ Test/JS/main.js
@@ -0,0 +1,26 @@
+// function puush(){
+//     document.getElementById('button').style.backgroundColor = '#817F7FFF';
+//     var abc = document.querySelectorAll('.catalog__col');
+//     for(var i = 0; i < abc.length; i++) {
+//         abc[i].style.width="100%";
+//     }
+// }
+
+
+function puush() {
+    if (document.getElementById('button').style.backgroundColor ==='rgb(129, 127, 127)') {
+        document.getElementById('button').style.backgroundColor = '#FFF';
+        var abc = document.querySelectorAll('.catalog__col');
+        for (var i = 0; i < abc.length; i++) {
+            abc[i].classList.remove('catalog__full');
+        }
+    } else {
+        document.getElementById('button').style.backgroundColor = '#817F7FFF';
+        var abc = document.querySelectorAll('.catalog__col');
+        for (var i = 0; i < abc.length; i++) {
+            abc[i].classList.add('catalog__full');
+        }
+    }
+}
+
+// abc[i].classList.remove('catalog__full');
